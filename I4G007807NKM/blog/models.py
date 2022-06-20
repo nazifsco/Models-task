f8ecea133e75546c26ef8598fc4fe798d955ac20
Nazeef Danladi Adamu <105206505+nazifsco@users.noreply.github.com>
2022-06-20 05:00:26 -1200

    initial

--- I4G007807NKM/blog/models.py
+++ I4G007807NKM/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
     Created_date = models.DateTimeField()
     Published_date = models.DateTimeField()
 
